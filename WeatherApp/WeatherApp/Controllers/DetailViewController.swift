7372a260defe6bf7fcbdbb79c8753436fd2a7351
Krystal Campbell <krystalcampbell@Krystals-MBP.home>
2020-03-02 16:56:16 -0500

    Fixes constraints on UI elements, updates information in labels and text views, & sets up functionality for objc save button

--- WeatherApp/WeatherApp/Controllers/DetailViewController.swift
+++ WeatherApp/WeatherApp/Controllers/DetailViewController.swift
@@ -10,102 +10,116 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    var weatherDeets: WeatherData!
+    
     var pictures = [Hit](){
         didSet {
-           
+            loadData()
         }
     }
     
     var cityName:String! {
         didSet {
             loadData()
-          }
-      }
-    
-    var weatherDeets: WeatherData!
+            cityForecastDateLabel.text = "Weather Forecast for \(self.cityName.description), Time: \(weatherDeets.getSpecificTimeFromTime(time: weatherDeets.time))"
+        }
+    }
     
     lazy var cityForecastDateLabel: UILabel = {
-          let label = UILabel()
-          label.textAlignment = .center
-          label.font = UIFont(name: "Marker Felt", size: 30.0)
-          label.textColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
-          label.backgroundColor = .systemGreen
-          return label
-          }()
-      
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        //label.text = "Weather for:"
+        label.font = UIFont(name: "Marker Felt", size: 14.0)
+        label.textColor = .black
+        label.layer.borderWidth = 3
+        label.layer.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        label.backgroundColor = .clear
+        return label
+    }()
+    
     
     lazy var cityImage: UIImageView = {
         let iv = UIImageView()
         iv.clipsToBounds = true
         iv.contentMode = .scaleAspectFill
-        iv.layer.borderWidth = 4
-        iv.layer.borderColor = UIColor.blue.cgColor
+        iv.layer.borderWidth = 3
+        iv.layer.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         return iv
-      }()
+    }()
     
-    lazy var descriptionTextView: UITextView = {
-        let label = UITextView()
-        label.textAlignment = .center
-        label.font = UIFont(name: "Marker Felt", size: 30.0)
-        label.textColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
-        label.backgroundColor = .systemGreen
-        return label
-           }()
+    lazy var descriptionLabel: UILabel = {
+        let tv = UILabel()
+        tv.textAlignment = .center
+        tv.numberOfLines = 0
+        tv.adjustsFontSizeToFitWidth = true
+        //tv.text = "Enter Description Here "
+        tv.font = UIFont(name: "Marker Felt", size: 20.0)
+        tv.backgroundColor = .clear
+        tv.layer.borderWidth = 2
+        tv.layer.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        return tv
+    }()
     
     lazy var saveButton: UIBarButtonItem = {
         let button = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.save, target: self, action: #selector(saveButtonPressed))
-           button.isEnabled = false
+        button.isEnabled = false 
         return button
-
+        
     }()
     
-    @objc func saveButtonPressed(){
-        let savedImage = FavoritedPictures(hits: pictures)
-        DispatchQueue.global(qos: .utility).async {
-            try?
-                ImagePersistenceManager.manager.saveImage(image: savedImage)
-//            DispatchQueue.main.async {
-//                self.navigationController?.popViewController(animated: true)
-            self.saveAlert()
-            }
-        }
- //   }
-    
-    private func saveAlert() {
-          let alert = UIAlertController(title: "Saved", message: "Click on the 'favorites' tag to see your saved images", preferredStyle: .alert)
-          let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
-          alert.addAction(ok)
-          self.present(alert,animated: true)
-      }
- 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         self.navigationItem.rightBarButtonItem = saveButton
         addSubViews()
         setUpConstraints()
-
+        setupLabels()
+        
     }
     
-  //MARK: -Private Functions
+    @objc func saveButtonPressed(){
+        let savedImage = FavoritedImages(image: (cityImage.image?.pngData())!)
+        try?
+        ImagePersistenceManager.manager.saveImage(image: savedImage)
+        self.saveAlert()
+    }
+    
+  
+    
+    private func setupLabels(){
+        descriptionLabel.text = "High: \(weatherDeets.temperatureHigh.description), Low: \(weatherDeets.temperatureLow.description), Sunrise: \(weatherDeets.sunriseTime), Sunset: \(weatherDeets.sunsetTime),             Rain %: \(weatherDeets.precipProbability), WindSpeed: \(weatherDeets.windSpeed)"
+    }
+    
+    private func saveAlert() {
+        let alert = UIAlertController(title: "Saved", message: "Click on the 'favorites' tag to see your saved images", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(ok)
+        self.present(alert,animated: true)
+    }
+    
+    
+    
+    
+    //MARK: -Private Functions
     
     private func addSubViews() {
         view.addSubview(cityForecastDateLabel)
         view.addSubview(cityImage)
-        view.addSubview(descriptionTextView)
+        view.addSubview(descriptionLabel)
     }
-      
-   
+    
+    
     private func loadData() {
-           PicturesAPIClient.shared.getPictures(searchTerm: cityName!){
-               (results) in
-               DispatchQueue.main.async {
-                   switch results {
-                   case .failure(let error):
-                       print(error)
-                   case .success(let data):
+        PicturesAPIClient.shared.getPictures(searchTerm: cityName!){
+            (results) in
+            DispatchQueue.main.async {
+                switch results {
+                case .failure(let error):
+                    print(error)
+                case .success(let data):
                     self.pictures = data
-               
+                    
                     ImageHelper.shared.getImage(urlStr: self.pictures.first?.largeImageURL ?? "") { (result) in
                         DispatchQueue.main.async {
                             switch result {
@@ -113,43 +127,50 @@ class DetailViewController: UIViewController {
                                 print(error)
                             case .success(let image):
                                 self.cityImage.image = image
+                                self.saveButton.isEnabled = true
                             }
                         }
                     }
                 }
-                    
+                
             }
-                    
-                }
-            }
+            
+        }
+    }
+    
+    //MARK: -Constraints Functions
+    
+    private func setUpConstraints() {
+        cityForecastDateLabelConstraints()
+        cityImageConstraints()
+        descriptionTextViewConstraints()
+    }
+    
+    private func cityForecastDateLabelConstraints() {
         
-           
-       
+        cityForecastDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityForecastDateLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 25).isActive = true
+        cityForecastDateLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+        cityForecastDateLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+        cityForecastDateLabel.heightAnchor.constraint(equalToConstant: 55).isActive = true
+    }
+    private func cityImageConstraints(){
+        
+        cityImage.translatesAutoresizingMaskIntoConstraints = false
+        cityImage.topAnchor.constraint(equalTo: cityForecastDateLabel.bottomAnchor, constant: 50).isActive = true
+        cityImage.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 25).isActive = true
+        cityImage.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -25).isActive = true
+        cityImage.heightAnchor.constraint(equalToConstant: 250).isActive = true
+    }
     
-  //MARK: -Constraints Functions
- private func setUpConstraints() {
-         
-         cityForecastDateLabel.translatesAutoresizingMaskIntoConstraints = false
-        cityForecastDateLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant:15).isActive = true
-       cityForecastDateLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
-         cityForecastDateLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
-         cityForecastDateLabel.heightAnchor.constraint(equalToConstant: 35).isActive = true
-    
-         cityImage.translatesAutoresizingMaskIntoConstraints = false
-         cityImage.topAnchor.constraint(equalTo: cityForecastDateLabel.bottomAnchor, constant: 30).isActive = true
-         cityImage.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 15).isActive = true
-        cityImage.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
-         cityImage.heightAnchor.constraint(equalToConstant: 100).isActive = true
-         
-         
-         
-         descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
-         
-         descriptionTextView.topAnchor.constraint(equalTo:  cityImage.bottomAnchor, constant: 30).isActive = true
-        descriptionTextView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 70).isActive = true
-         descriptionTextView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -70).isActive = true
-         descriptionTextView.heightAnchor.constraint(equalToConstant: 35).isActive = true
+    private func descriptionTextViewConstraints(){
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        descriptionLabel.topAnchor.constraint(equalTo: cityImage.bottomAnchor, constant: 50).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 70).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -70).isActive = true
+        descriptionLabel.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
 }
-    
- 
+
+
