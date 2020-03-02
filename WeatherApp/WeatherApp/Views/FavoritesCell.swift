9d0e4af245f76367ab2a13777f8ff9abe4cec981
Krystal Campbell <krystalcampbell@Krystals-MBP.home>
2020-03-02 17:00:11 -0500

    Sets up tableview cell for Favorite Tableview, adds constraints for image view inside of cell

--- WeatherApp/WeatherApp/Views/FavoritesCell.swift
+++ WeatherApp/WeatherApp/Views/FavoritesCell.swift
@@ -9,25 +9,34 @@
 import UIKit
 
 class FavoritesCell: UITableViewCell {
-
-    lazy var favoritesCityImage: UIImageView = {
-           let iv = UIImageView()
-           iv.clipsToBounds = true
-           iv.contentMode = .scaleAspectFill
-           iv.layer.borderWidth = 4
-           iv.layer.borderColor = UIColor.blue.cgColor
-           return iv
-         }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    lazy var favoritesCityImage: UIImageView = {
+        let iv = UIImageView()
+        iv.clipsToBounds = true
+        iv.contentMode = .scaleAspectFill
+        iv.layer.borderWidth = 4
+        iv.layer.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        return iv
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style:style, reuseIdentifier: reuseIdentifier)
+        setupView()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    
+    private func setupView() {
+        contentView.addSubview(favoritesCityImage)
+        favoritesCityImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        favoritesCityImage.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        favoritesCityImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:  10).isActive = true
+        favoritesCityImage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+        favoritesCityImage.widthAnchor.constraint(equalTo: self.contentView.widthAnchor).isActive = true
+    }
 }
